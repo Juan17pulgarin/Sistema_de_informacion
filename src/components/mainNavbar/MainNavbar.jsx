e5227535241da6427ab8a6a1ceb2b1db815ffe00
Haiber montaña <haiber.montanat@gmail.com>
2023-11-14 17:55:50 -0500

    ..

--- src/components/mainNavbar/MainNavbar.jsx
+++ src/components/mainNavbar/MainNavbar.jsx
@@ -4,7 +4,7 @@ import configu from '../../assets/img/configuraciones_1.png';
 import recoge from '../../assets/img/maps_and_location_1.png';
 import usua from '../../assets/img/usuario.png';
 import { Link } from 'react-router-dom';
-import './mainNavbar.css';
+
 
 const MainNavbar = () => {
   return (
