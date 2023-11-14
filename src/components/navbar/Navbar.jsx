8b71640b0bd752e973a11d430f86f7898378a415
Haiber montaña <haiber.montanat@gmail.com>
2023-11-14 17:30:33 -0500

    Coreccion hoja de estilo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import './navbar.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png'
 
